cd20a507d130d68979767a5b520d577b96ebe20e
Davoud Qaragouzlou <davoodgh0251@gmail.com>
2023-05-30 14:58:15 +0430

    Setting up Reusable Logic

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+
+import '../app/globals.css';
+import { Navbar, Footer } from '@/components';
+
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+  <div className='dark:bg-nft-dark bg-white min-h-screen'>
+    <Navbar />
+    <Component {...pageProps} />
+    <Footer />
+  </div>
+  <Script src="https://kit.fontawesome.com/45c1f707a5.js" crossorigin="anonymous"></Script>
+  </ThemeProvider>
+);
+
+export default MyApp;
