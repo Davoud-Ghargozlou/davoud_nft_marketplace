7f8836e69730c6f918bd5d419722e73a8515da44
Davoud Qaragouzlou <davoodgh0251@gmail.com>
2023-06-10 17:16:25 +0430

    MetaMask wallet Connected

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,6 +4,7 @@ import { useTheme } from 'next-themes';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { NFTContext } from '@/context/NFTContext';
 import images from '../assets/assets';
 import Button from './Button';
 
@@ -52,9 +53,9 @@ const MenuItems = ({ isMobile, active, setActive, setIsOpen }) => {
 };
 
 const ButtonGroup = ({ setActive, router }) => {
-  const hasConnected = false;
+  const { connectWallet, currentAccount } = useContext(NFTContext);
 
-  return hasConnected ? (
+  return currentAccount ? (
     <Button
       btnName="Create"
       classStyles="mx-2 rounded-xl"
@@ -67,7 +68,7 @@ const ButtonGroup = ({ setActive, router }) => {
     <Button
       btnName="Connect"
       classStyles="mx-2 rounded-xl"
-      handleClick={() => {}}
+      handleClick={connectWallet}
     />
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,26 @@
 import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
 
+import { NFTProvider } from '@/context/NFTContext';
 import '../app/globals.css';
 import { Navbar, Footer } from '@/components';
 
 const MyApp = ({ Component, pageProps }) => (
-  <ThemeProvider attribute="class">
-    <div className="dark:bg-nft-dark bg-white min-h-screen">
-      <Navbar />
-      <div className="pt-65">
-        <Component {...pageProps} />
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <div className="pt-65">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-    <Script
-      src="https://kit.fontawesome.com/45c1f707a5.js"
-      crossorigin="anonymous"
-    ></Script>
-  </ThemeProvider>
+      <Script
+        src="https://kit.fontawesome.com/45c1f707a5.js"
+        crossorigin="anonymous"
+      ></Script>
+    </ThemeProvider>
+  </NFTProvider>
 );
 
 export default MyApp;
